ca709fc520798a2c9689356180b3e8d618dc080e
Jan Täubrich <jantaeubrich@freenet.de>
2026-08-13 06:16:33 +0200

    Änderungen im Speicherverhalten im Safari.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'handballtrainer-v63-1';
+const CACHE_NAME = 'handballtrainer-v64-1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -9,9 +9,7 @@ const APP_SHELL = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
@@ -26,12 +24,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        const copy=response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy));
         return response;
       })
       .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
